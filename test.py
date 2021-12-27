ed19ce8a78836cff657db90f528be6f294ea0b81
Karsten Neumann <karsten.neumann@slm-solutions.com>
2021-12-27 21:41:59 +0100

    Removed some code

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
-import os
-import sys
-from os import rename
-
-#import matplotlib
-import requests
-
-r = requests.get("https://coreyms.com")
-print(r.status_code)
-test = "Test"
 name = input('your Name?')
 print(f'Hello {name}')
